14c3d2b51e07a66c1f81f36d57bd861152af8aad
eve-2525 <eve.eve.love.0625@gmail.com>
2022-05-04 22:41:19 +0900

    ActiveHashアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,14 @@
 class Item < ApplicationRecord
 
   has_one_attached :image
-
   belongs_to:user
 
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :area
+  belongs_to :number_of_days
+
+
 end
