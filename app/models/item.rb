a9c0a465aeab017e08e9d9cc6966ce09bc6d4b24
eve-2525 <eve.eve.love.0625@gmail.com>
2022-05-04 23:00:07 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,20 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to:user
 
+  validates :product, presence: true
+  validates :product_description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :postage_id, presence: true
+  validates :area_id, presence: true
+  validates :number_of_days_id, presence: true
+  validates :price, presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :postage
   belongs_to :area
   belongs_to :number_of_days
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-
   validates :birthday, presence: true
-
 
   has_many:items
 
