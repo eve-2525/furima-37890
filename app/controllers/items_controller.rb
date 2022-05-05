99d80eeda6bdf25566c0c1cf1320a04e4c600594
eve-2525 <eve.eve.love.0625@gmail.com>
2022-05-05 15:52:29 +0900

    商品出品ページへの移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [ :destroy, :new, :edit]
+
   def index
   end
 
   def new
-    @item = Iitem.all
   end
-  
+
   def create
   end
 
