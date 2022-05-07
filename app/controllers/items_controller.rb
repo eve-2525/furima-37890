373fe1bbdd7c468f1bee61f2937a5c926c88191b
eve-2525 <eve.eve.love.0625@gmail.com>
2022-05-07 13:11:57 +0900

    updateアクション 条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :set_item, only: [:edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all
@@ -17,6 +19,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+  
+      else
+      render :edit
+      end
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -28,8 +42,21 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:product, :product_description, :category_id, :condition_id, :postage_id, :area_id, :number_of_days_id,
                                  :price, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    if  user_signed_in? && current_user.id == @item.user_id
+    elsif user_signed_in? && current_user.id != @item.user_id
+      redirect_to action: :index
+    else redirect_to action: :new
+    end
+  end
+
 end
