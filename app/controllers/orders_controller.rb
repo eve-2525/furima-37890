b777eca8b94621bf28f4f4e051ac6af496882a56
eve-2525 <eve.eve.love.0625@gmail.com>
2022-05-07 23:29:08 +0900

    ordersコントローラー,アクション定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,13 @@
 class OrdersController < ApplicationController
 
-  
+  def index
+    @order = order.new
+  end
+
+  def create
+    # @donation = Donation.create(donation_params)
+    # Address.create(address_params)
+    # redirect_to root_path
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class クラス名
   include ActiveModel::Model
   attr_accessor :user_id,:item_id,:postal_code,:area_id,:city,:block_number,:building,:telephone_number,:purchase_record
-  # ここにバリデーションの処理を書く
+
 
   validates :user_id, presence: true
   validates :item_id, presence: true
@@ -22,10 +22,9 @@ class クラス名
   end
 
 def save
-    # 寄付情報を保存し、変数donationに代入する
+
     purchase_records = Purchase_records.create(item_id: item_id, user_id: user_id)
-    # 住所を保存する
-    # donation_idには、変数donationのidと指定する
+
     Shipping_addresses.create(postal_code: postal_code, area_id: area_id, city: city, block_number: block_number, building: building, telephone_number: telephone_number, purchase_record: purchase_record)
   end
 end
