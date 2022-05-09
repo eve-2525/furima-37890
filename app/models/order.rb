36eb4f103607fd2b61dcf7272d2a5ce31534dc13
eve-2525 <eve.eve.love.0625@gmail.com>
2022-05-09 14:52:46 +0900

    エラーメッセージ修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,19 +3,21 @@ class Order
   attr_accessor :user_id,:item_id,:postal_code,:area_id,:city,:block_number,:building,:telephone_number,:purchase_record
 
 
+  with_options presence: true,
+  format: { with: /[0-9]{3}-[0-9]{4}/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' } do
+validates :postal_code
+end
+
   validates :user_id, presence: true
   validates :item_id, presence: true
-  validates :area_id, presence: true
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city, presence: true
   validates :block_number, presence: true
   # validates :purchase_record_id, presence: true
 
 
 
-  with_options presence: true,
-  format: { with: /[0-9]{3}-[0-9]{4}/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' } do
-validates :postal_code
-end
+
 
   with_options presence: true,
                format: { with: /0\d{9,10}/, message: 'is invalid. Input only number' } do
